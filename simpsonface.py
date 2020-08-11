136b0421b492e92756df3433bae1d211a91af6da
Sampo Pelto <38130111+sampozki@users.noreply.github.com>
2020-08-11 17:36:15 +0300

    morbotron support

--- simpsonface.py
+++ simpsonface.py
@@ -4,9 +4,9 @@ import asyncio
 import json
 
 
-async def sendface(message):
-    response = urllib3.PoolManager().request( "GET", "https://frinkiac.com/api/random")
+async def sendface(message, urll):
+    response = urllib3.PoolManager().request( "GET", "https://" + urll + ".com/api/random")
     data = json.loads(response.data.decode("utf-8"))
 
-    url = "https://frinkiac.com/img/" + data['Frame']['Episode'] + "/" + str(data['Frame']['Timestamp']) + ".jpg"
+    url = "https://" + urll + ".com/img/" + data['Frame']['Episode'] + "/" + str(data['Frame']['Timestamp']) + ".jpg"
     await message.channel.send(url)
