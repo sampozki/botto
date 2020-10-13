f3203bb164385faf6979990ddd0b90c77130fbea
Sampo Pelto <pelto.sampo@gmail.com>
2020-10-13 21:53:04 +0300

    Removed rigging

--- simpsonface.py
+++ simpsonface.py
@@ -6,15 +6,8 @@ import json
 
 async def sendface(message, urll):
     
-    # +4
-    if message.author.id == 11976700001150566:
-        pass
-        #    await message.channel.send("https://masterofallscience.com/img/S03E03/56848.jpg")
-    
-    else:
+    response = urllib3.PoolManager().request( "GET", "https://" + urll + ".com/api/random")
+    data = json.loads(response.data.decode("utf-8"))
 
-        response = urllib3.PoolManager().request( "GET", "https://" + urll + ".com/api/random")
-        data = json.loads(response.data.decode("utf-8"))
-
-        url = "https://" + urll + ".com/img/" + data['Frame']['Episode'] + "/" + str(data['Frame']['Timestamp']) + ".jpg"
-        await message.channel.send(url)
+    url = "https://" + urll + ".com/img/" + data['Frame']['Episode'] + "/" + str(data['Frame']['Timestamp']) + ".jpg"
+    await message.channel.send(url)
