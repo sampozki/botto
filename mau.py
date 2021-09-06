9e1a31613bcb77d69912d605066b9e8d5d7709f8
Sampo Pelto <pelto.sampo@gmail.com>
2021-09-07 01:00:42 +0300

    pahan mielen grumpy cat blocked af

--- mau.py
+++ mau.py
@@ -9,6 +9,8 @@ async def mau(message):
         response= urllib3.PoolManager().request('GET','http://aws.random.cat/meow')
         data = json.loads(response.data.decode('utf-8'))
 
+        
+
         await message.channel.send(str(data['file']).replace(' ', '%20'))
     except Exception as e:
         print(e)
@@ -17,7 +19,13 @@ async def mau(message):
 async def mau2(message):
     response= urllib3.PoolManager().request("GET","https://api.thecatapi.com/v1/images/search")
     data = json.loads(response.data.decode("utf-8"))
-    await message.channel.send(str(data[0]["url"]).replace(" ", "%20"))
+
+    responseUrl = str(data[0]["url"]).replace(" ", "%20")
+    if responseUrl == "https://cdn2.thecatapi.com/images/sqb5Hk8Ck.jpg":
+        responseUrl = 'Pahan mielen grumpy cat'
+        await mau2(message)
+        return
+    await message.channel.send(responseUrl)
 
 async def mau3(message):
     await message.channel.send("https://thiscatdoesnotexist.com/")
